3d7a02db1bb170f97bb8fb52081a419b1195e1df
mohammad <m.a.taghva@gmail.com>
2021-06-12 21:58:33 -0700

    Complete backend user Auth

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
 mongoose
-.connect(db, { useNewUrlParser: true })
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello My World"));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello My World")
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
