d2b1f87673a6ed59d54e3311ba2c45d8921f3aaf
mohammad <m.a.taghva@gmail.com>
2021-06-14 01:07:38 -0700

    Finish Tweets

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import sessionApi from './session_api_reducer';
+import errors from './errors_reducer';
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
-  session
+  session,
+  sessionApi,
+  tweets: TweetsReducer,
+  errors
 });
 
 export default RootReducer;
